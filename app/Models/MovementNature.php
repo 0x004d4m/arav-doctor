0331e4bbb9e471eeae5e2d9b0ce59dd552a78145
0x004d4m <0x004d4m@gmail.com>
2022-07-14 14:26:08 +0300

    BasicSetUpAndDB

---
+++ app/Models/MovementNature.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MovementNature extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/SubscribtionStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubscribtionStatus extends Model
+{
+    use HasFactory;
+}
